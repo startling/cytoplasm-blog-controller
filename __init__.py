53b9e6a4021772aa1b5613cc5aa024e7b6d0e0ce
tim <tdixon51793@gmail.com>
2011-12-13 21:34:47 -0600

    Make an index.html file out of chronological.mako with all the posts.
    Coming soon:
    * Posts should be sorted by date and divided into multiple pages.
    * Posts should be placed in directories for their years and months
    * There should be and index.html for each of the year and month directories.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import os, cytoplasm, yaml
+from cytoplasm.interpreters import interpret
 
 def metadata(file):
     "Read the metadata for file `file` from file.yaml."
@@ -19,7 +20,7 @@ class Post(object):
         self.title = meta["title"]
         self.slug = self.title.replace(" ", "-")
         # Interpret the file.
-        cytoplasm.interpreters.interpret(self.path, self)
+        interpret(self.path, self)
 
     def close(self):
         # This is just here so that python doesn't throw up an error when something else thinks
@@ -31,17 +32,26 @@ class Post(object):
         self.contents = s 
 
 class BlogController(cytoplasm.controllers.Controller):
-    def __init__(self, data, destination, templates="_templates/"):
+    def __init__(self, data, destination, posts_per_page=10, templates="_templates/"):
         # take three arguments: the source directory, the destination directory, and, optionally,
-        # a directory wherein the templates reside.
+        # a directory wherein the templates reside and a number of posts per page.
         self.templates_directory = templates
+        self.posts_per_page = posts_per_page
         cytoplasm.controllers.Controller.__init__(self, data, destination)
         
     def __call__(self):
+        posts = []
         post_template = "%s/%s" %(self.templates_directory, "post.mako")
+        chronological_template = "%s/%s" %(self.templates_directory, "chronological.mako")
+        def chronological(directory, posts):
+            destination = "%s/index.html" %(directory)
+            interpret(chronological_template, destination, posts=posts, next=None, previous=None)
+        
         for post in [p for p in os.listdir(self.data_directory) if not p.endswith(".yaml")]:
             this_post = Post("%s/%s" %(self.data_directory, post))
             destination = "%s/%s" %(self.destination_directory, "%s.html" %(this_post.slug))
-            cytoplasm.interpreters.interpret(post_template, destination, post=this_post)
+            interpret(post_template, destination, post=this_post)
+            posts.append(this_post)
+        chronological(self.destination_directory, posts)
 
 info = { "class" : BlogController }

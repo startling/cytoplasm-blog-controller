5b1b1d0ade40bf1b0cf908dafb11331defeae578
tim <tdixon51793@gmail.com>
2011-12-13 18:43:45 -0600

    For each post in the source directory, make a Post object.
    This Post object gets its contents by calling the interpret method
    (from cytoplasm.interpreters) on itself, and giving itself as the
    file object to be written to.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,28 @@
-from cytoplasm.controllers import Controller
+import os, cytoplasm 
 
 class Post(object):
-    pass
+    "A sort-of file-like object that defines a post."
+    def __init__(self, path):
+        self.path = path
+        # The following are to be deduced somehow from metadata; they don't do anything yet.
+        self.date = None
+        self.contents = None
+        self.title = None
+        # Interpret the file.
+        cytoplasm.interpreters.interpret(self.path, self)
 
-class BlogController(Controller):
-    pass
+    def close(self):
+        # This is just here so that python doesn't throw up an error when something else thinks
+        # this is a file.
+        pass
+
+    def write(self, s):
+        # instead of writing to disk, simply change the contents attribute.
+        self.contents = s 
+
+class BlogController(cytoplasm.controllers.Controller):
+    def __call__(self):
+        for post in os.listdir(self.data_directory):
+            this_post = Post("%s/%s" %(self.data_directory, post))
 
 info = { "class" : BlogController }

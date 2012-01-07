f681142442cd3b67363f0acc6248a5534ea2dcc4
tim <tdixon51793@gmail.com>
2012-01-07 01:13:42 -0600

    Made everythign PEP8 compliant!
    I'm going to sleep.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
-import os, cytoplasm, yaml, re, datetime
+import os
+import yaml
+import re
+import datetime
+import cytoplasm
 from operator import attrgetter
 from collections import defaultdict
 from cytoplasm.interpreters import interpret, interpret_to_filelike
 from cytoplasm.errors import ControllerError
+
 
 def metadata(file):
     "Read the metadata for file `file` from file.yaml."
@@ -12,19 +17,24 @@ def metadata(file):
     # get everything within comments that look like:
     # <!-- metadata
     # -->
-    commented = re.match(r'<!-- metadata\n(.*?)\n-->.*', contents, flags=re.DOTALL)
+    commented = re.match(r'<!-- metadata\n(.*?)\n-->.*', contents,
+            flags=re.DOTALL)
     # if there's no match, raise an error.
-    if commented == None: 
-        raise ControllerError("Post '%s' has no commented metadata." %(file))
-    # otherwise, get yaml data from the first matching group (there should be only one anyway).
+    if commented == None:
+        raise ControllerError("Post '%s' has no commented metadata." % (file))
+    # otherwise, get yaml data from the first matching group (there should be
+    # only one anyway).
     meta = yaml.load(commented.group(1))
     # raise an error if there's no title in the metadata
     if "title" not in meta.keys():
-        raise ControllerError("Post '%s' doesn't have a title in its metadata." %(file))
+        raise ControllerError("Post '%s' doesn't have a title in its metadata."
+                % (file))
     # raise an error if there's no date in the metadata:
     if "date" not in meta.keys():
-        raise ControllerError("Post '%s' doesn't have a date in its metadata." %(file))
+        raise ControllerError("Post '%s' doesn't have a date in its metadata."
+                % (file))
     return meta
+
 
 class Post(object):
     "A sort-of file-like object that defines a post."
@@ -35,42 +45,54 @@ class Post(object):
         self.contents = None
         self.author = None
         self.email = None
-        # an empty list for tags; this way, if they aren't in the metadata, it defaults to this.
+        # an empty list for tags; this way, if they aren't in the metadata,
+        # it defaults to this.
         self.tags = []
-        # Read the metadata from this file and update this objects __dict__ with it;
-        # this allows the user to have arbitrary, custom fields further than "title" and "date".
+        # Read the metadata from this file and update this objects __dict__
+        # with it; this allows the user to have arbitrary, custom fields
+        # further than "title" and "date".
         self.__dict__.update(metadata(self.path))
         # get a datetime object from the "date" metadata
         self.date = datetime.datetime.strptime(self.date, "%Y/%m/%d")
         # get these to be nice...
-        self.year, self.month, self.day = self.date.year, self.date.month, self.date.day
-        # This is a whitespace-free version of the name, to be used in things like filenames.
+        self.year = self.date.year
+        self.month = self.date.month
+        self.day = self.date.day
+        # This is a whitespace-free version of the name, to be used in things
+        # like filenames.
         self.slug = self.title.replace(" ", "-")
-        # this is the relative url for the post, relative from the destination directory:
+        # this is the relative url for the post, relative from the destination
+        # directory:
         self.url = os.path.join(str(self.year), str(self.month), self.slug)
         # Interpret the file.
         interpret_to_filelike(self.path, self)
 
     def close(self):
-        # This is just here so that python doesn't throw up an error when something else thinks
-        # this is a file.
+        # This is just here so that python doesn't throw up an error when
+        # something else thinks this is a file.
         pass
 
     def write(self, s):
-        # instead of writing to disk when this is written to , simply change the contents attribute.
-        self.contents = s.decode("utf8") 
+        # instead of writing to disk when this is written to , simply change
+        # the contents attribute.
+        self.contents = s.decode("utf8")
+
 
 class BlogController(cytoplasm.controllers.Controller):
-    def __init__(self, data, destination, templates="_templates", posts_per_page=10):
-        # take the base arguments for a controller and, optinally, the number of posts per page.
+    def __init__(self, data, destination, templates="_templates",
+            posts_per_page=10):
+        # take the base arguments for a controller and, optinally, the number
+        # of posts per page.
         self.posts_per_page = posts_per_page
     # pass the base arguments to the base controller's __init__
-        cytoplasm.controllers.Controller.__init__(self, data, destination, templates)
-        
+        cytoplasm.controllers.Controller.__init__(self, data, destination,
+                                                    templates)
+
     def __call__(self):
-        # this is a dictionary where the values are lists of Post objects and the keys are the 
-        # directories where those posts should go. For example, divisions["2011"] will have all
-        # the things posted in 2011. There will be months, too, like divisions["2011/12"]. 
+        # this is a dictionary where the values are lists of Post objects and
+        # the keys are the directories where those posts should go. For
+        # example, divisions["2011"] will have all the things posted in 2011.
+        # There will be months, too, like divisions["2011/12"].
         # Everything will be in divisions[""].
         divisions = defaultdict(list)
         # figure out the templates to use:
@@ -83,55 +105,74 @@ class BlogController(cytoplasm.controllers.Controller):
             divisions[""].append(post)
             # append it to the list of posts for its year and month.
             divisions[str(post.year)].append(post)
-            divisions[os.path.join(str(post.year), str(post.month))].append(post)
+            divisions[os.path.join(str(post.year), str(post.month))].append(
+                                                                        post)
             # append the post to a division for each of its tags
             for tag in post.tags:
                 divisions[tag].append(post)
-        # for each of the keys in divisions, make chronological pages for that directory.
-        # (this is sorted so that, for example, "2011" goes before "2011/12". If it weren't sorted,
-        # you'd get an error because the directory "2011/12" can't be created before the directory
-        # "2011".)
+        # for each of the keys in divisions, make chronological pages for that
+        # directory. (this is sorted so that, for example, "2011" goes before
+        # "2011/12". If it weren't sorted, you'd get an error because the
+        # directory "2011/12" can't be created before the directory "2011".)
         for directory in sorted(divisions.keys()):
             # get the list of posts
             posts = divisions[directory]
             # get the real directory by prepending the destination_directory
             directory = os.path.join(self.destination_directory, directory)
             # if the directory doesn't exist, create it.
-            if not os.path.exists(directory): os.mkdir(directory)
-            # sort posts by the year, then month, then day, with the most recent first.
+            if not os.path.exists(directory):
+                os.mkdir(directory)
+            # sort posts by the year, then month, then day, with the most
+            # recent first.
             posts.sort(key=attrgetter('year', 'month', 'day'), reverse=True)
             # the number of pages
             number = len(posts) // self.posts_per_page
             # if there are any remainders, add another page for them.
-            if number * self.posts_per_page < len(posts): number += 1
+            if number * self.posts_per_page < len(posts):
+                number += 1
             # make the pages:
             for n in range(number):
                 # if this is the first page, name it "index.html"
-                if n == 0: name = os.path.join(directory, "index.html")
-                else: name = os.path.join("%s", "%d.html" %(directory, n))
-                # if this isn't page 0, previous should link to the page before
-                if n == 0: prev = None
-                elif n == 1: prev = "index.html"
-                else: prev = "%d.html" %(directory, n - 1)
-                # if this isn't the last page, next should link to the next page...
-                if n == number - 1: next = None
-                else: next = "%d.html" %(n + 1)
-                # slice the list of posts according to how many should be on this page
-                p = posts[n * self.posts_per_page:(n + 1) * self.posts_per_page]
-                interpret(chronological_template, name, posts=p, next=next, previous=prev)
-        # for each of the posts, apply the mako template and write it to a file.
+                if n == 0:
+                    name = os.path.join(directory, "index.html")
+                else:
+                    name = os.path.join("%s", "%d.html" % (directory, n))
+                # if this isn't page 0, previous should link previous page.
+                if n == 0:
+                    prev = None
+                elif n == 1:
+                    prev = "index.html"
+                else:
+                    prev = "%d.html" % (directory, n - 1)
+                # if this isn't the last, next should link to the next page.
+                if n == number - 1:
+                    next = None
+                else:
+                    next = "%d.html" % (n + 1)
+                # slice the list of posts according to how many should be on
+                # this page
+                p = posts[n * self.posts_per_page:(n + 1) *
+                            self.posts_per_page]
+                interpret(chronological_template, name, posts=p, next=next,
+                            previous=prev)
+        # for each of the posts, apply the mako template and write it to a
+        # file.
         for post in divisions[""]:
             destination = os.path.join(self.destination_directory, post.url)
             # interpret the post
             interpret(post_template, destination, post=post)
-        # finally, for each of the templates in the templates/feeds directory, give them the posts.
+        # finally, for each of the templates in the templates/feeds directory,
+        # give them the posts.
         feed_dir = os.path.join(self.templates_directory, "feeds")
         if os.path.exists(feed_dir):
             for feed in (os.listdir(feed_dir)):
-                # save the output to the name of the file without the last suffix
-                destination = os.path.join(self.destination_directory, ".".join(feed.split(".")[:-1]))
+                # save the output to the name of the file without the
+                # last suffix
+                destination = os.path.join(self.destination_directory,
+                        ".".join(feed.split(".")[:-1]))
                 # interpret this template, given the list of posts
-                interpret(os.path.join(feed_dir, feed), destination, posts=divisions[""])
+                interpret(os.path.join(feed_dir, feed), destination,
+                                                    posts=divisions[""])
 
 
-info = { "class" : BlogController }
+info = {"class": BlogController}
--- test_blog.py
+++ test_blog.py
@@ -1,12 +1,16 @@
-import os, unittest
+import os
+import unittest
 import cytoplasm
 from cytoplasm.test_build import Base
 
+
 test_site = os.path.join(os.path.dirname(__file__), "test")
+
 
 class TestBlogController(Base):
     def setUp(self):
         Base.setUp(self, test_site)
+
 
 if __name__ == '__main__':
     unittest.main()
